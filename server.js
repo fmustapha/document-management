d55848be8910416b242463353d06a7247dfa7189
Faith Mustapha <faith.mustapha@andela.com>
2017-04-22 19:15:09 +0100

    chore(.gitignore, .sequelizerc, package.json, packa ge.json, server.js, server/config/app.js): update files [#144111965]

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+import http from 'http';
+import dotenv from 'dotenv';
+import app from './server/config/app';
 
-// Set up the express app
-const app = express();
-
-// Log requests to the console.
-app.use(logger('dev'));
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the end of nothingness.',
-}));
-
-
-app.listen(5000, () => {
-  console.log('server started 5000');
+dotenv.config();
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`listening at ${port}`);
 });
-module.exports = app;
--- server/config/app.js
+++ server/config/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import userRouter from './routes/User';
-import docRouter from './routes/Document';
-import roleRouter from './routes/Role';
+// import userRouter from './routes/User';
+// import docRouter from './routes/Document';
+// import roleRouter from './routes/Role';
 
 // Set up the express app
 const app = express();
@@ -19,9 +19,9 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: 'Welcome to Document Management System' });
 });
 
-app.use('/users', userRouter);
-app.use('/documents', docRouter);
-app.use('/roles', roleRouter);
+// app.use('/users', userRouter);
+// app.use('/documents', docRouter);
+// app.use('/roles', roleRouter);
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'REQUEST PAGE NOT FOUND' });
