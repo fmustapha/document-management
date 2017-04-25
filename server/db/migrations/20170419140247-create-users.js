885e81c770ee9dc7670ead1cf9d0066ae1e20c54
Faith Mustapha <faith.mustapha@andela.com>
2017-04-25 04:28:49 +0100

    chore(migrations/...create-users.js): add migration file for users to project [#144210907]

---
+++ server/db/migrations/20170419140247-create-users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    firstname: {
+      type: Sequelize.STRING
+    },
+    lastname: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    roleId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Roles',
+        key: 'id',
+      }
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
+};
