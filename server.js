21134c06e6a7ca95695d93614c97c6a574349d1a
Faith Mustapha <faith.mustapha@andela.com>
2017-05-01 01:43:41 +0100

    feature(server.js): import and use js-logger in file for project [#144369551]

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import http from 'http';
 import dotenv from 'dotenv';
-// import logger from 'morgan';
+import Logger from 'js-logger';
 import app from './server/config/app';
+import database from './server/app/models';
 
 dotenv.config();
+Logger.useDefaults();
+
 const port = parseInt(process.env.PORT, 10) || 8000;
+
 app.set('port', port);
+
 const server = http.createServer(app);
-server.listen(port, () => {
-  // logger('Connected');
-});
+
+database.sequelize.sync().then(db => (db ? server
+.listen(port) : Logger.info('could not connect to database')))
+.then(() => Logger.info('server connected'))
+.catch(error => Logger.error(error));
+
