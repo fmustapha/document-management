79ec0d4d324459db3bc7f2135b1374cd8cf6729e
Faith Mustapha <faith.mustapha@andela.com>
2017-05-01 01:55:22 +0100

    feature(migrations/...create-users.js, create-roles.js, create-document.js): delete migration files and re-create them to reflect changes in the models [#144369551]

--- server/db/migrations/20170419143729-create-document.js
+++ server/db/migrations/20170419143729-create-document.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       content: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
