e60b4e7919efcb629acf17f9025da83d2f511483
Faith Mustapha <faith.mustapha@andela.com>
2017-05-04 15:22:13 +0100

    refactor(controllers/document.js): remove comment and replace getDocument function with findDocument function [#144369551]

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -1,12 +1,10 @@
 import db from '../models';
 import Helper from '../helper/Helper';
 
-// console.log(userModel)
 export default {
 
   createDocument(req, res) {
-    console.log(req.body, 'req.body');
-    db.Document.create(req.body)
+    return db.Document.create(req.body)
       .then(((newDocument) => {
         res.status(200)
           .send({
@@ -20,28 +18,86 @@ export default {
       });
   },
 
-  getDocument(req, res) {
-    res.send('Get all documents');
-    // return res.status(200)
-    //   .send({
-    //     message: 'You have successfully retrived this user',
-    //     user: Helper.getUserProfile(req.getUser)
-    //   });
-  },
-
-  getUserDocuments(req, res) {
-    res.send('Get documents for one user');
-  },
-
   findDocument(req, res) {
-    res.send('Find a particular document');
+    return db.Document.findById(req.params.id)
+      .then(document => res.status(200)
+        .send({
+          message: 'Successful',
+          document
+        }))
+      .catch(error => res.status(400).send({
+        error
+      }));
+  },
+
+  listDocuments(req, res) {
+    return db.Document.findAll({
+      offset: req.query.offset || 0,
+      limit: req.query.limit || 20,
+      include: [db.User],
+      order: [
+        ['updatedAt', 'DESC']
+      ]
+    })
+    .then(document => res.status(200)
+    .send({ message: 'Successfull', document }))
+    .catch(error => res.status(400).send({
+      error,
+      message: 'Error retrieving documents'
+    }));
   },
 
   modifyDocument(req, res) {
-    res.send('Make changes to a document');
+    db.Role.findById(req.decoded.data.roleId)
+    .then(() => db.Document
+        .find({ where: {
+          id: req.params.id } })
+          .then((document) => {
+            if (!document) {
+              return res.status(404).send({
+                message: 'Document Not Found',
+              });
+            }
+            if (Helper.isAdmin(req, res)
+            || Helper.isOwner(req, res, document)) {
+              return document
+              .update(req.body)
+              .then(updatedDoc => res.status(200).send({
+                updatedDoc,
+                message: 'Document updated successfully'
+              }));
+            }
+            return (res.status(403)
+               .send({ message: 'Unauthorized Access' }));
+          })
+          .catch(error => res.status(400).send({
+            error,
+            message: 'Error updating document'
+          })));
   },
 
   deleteDocument(req, res) {
-    res.send('Document has been successfully deleted');
+    db.Document
+      .find({
+        where: {
+          id: req.params.id
+        },
+      })
+      .then((document) => {
+        if (!document) {
+          return res.status(404).send({
+            message: 'Document Not Found',
+          });
+        }
+        return document
+          .destroy()
+          .then(() => res.status(200).send({
+            message: `${document.title}, has been successfully deleted`
+          }));
+      })
+      .catch(error => res.status(400).send({
+        error,
+        message: 'Error deleting document'
+      }));
   },
 };
