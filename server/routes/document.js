d92f003893096bea789f1d45457ec044e0b228ac
Faith Mustapha <faith.mustapha@andela.com>
2017-05-04 15:34:11 +0100

    refactor(routes/document.js): replace route [#144369551]

--- server/routes/document.js
+++ server/routes/document.js
@@ -4,7 +4,7 @@ import Documents from '../controllers/document';
 const document = express.Router();
 
 document.post('/', Documents.createDocument);
-document.get('/', Documents.getDocument);
+document.get('/', Documents.listDocuments);
 document.get('/:id', Documents.findDocument);
 document.put('/:id', Documents.modifyDocument);
 document.delete('/:id', Documents.deleteDocument);
