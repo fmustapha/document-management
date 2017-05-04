ab5321a945d34431554edb5e9967705ef318f3f0
Faith Mustapha <faith.mustapha@andela.com>
2017-05-04 13:48:54 +0100

    feature(controllers/search.js, routes/search.js): add search routes and controllers to project [#144369551]

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,7 +3,8 @@ import Search from '../controllers/search';
 
 const search = express.Router();
 
-// search.get('/', Search);
-search.get('/users/?q={username}', Search.search);
-// search.get('/documents/?q={doctitle}', Search.getOneDocument);
+search.get('/', Search.searchPage);
+search.get('/users', Search.userSearch);
+search.get('/documents', Search.documentSearch);
+
 export default search;
