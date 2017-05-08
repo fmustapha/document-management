2c90e63d598291efeadf5572b6afc8758045e8c0
Faith Mustapha <faith.mustapha@andela.com>
2017-05-08 16:50:09 +0100

    feature(controllers/document.js): fix houndci flag [Finishes #144369551]

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -63,7 +63,6 @@ export default {
                 message: 'Document Not Found',
               });
             }
-            console.log('document', document);
             if (Helper.isAdmin(req, res)
             || Helper.isOwner(req, res, document)) {
               return document
