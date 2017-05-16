ac64fdd0aaa6d36f826f2cfe5187b077a53a3387
Faith Mustapha <faith.mustapha@andela.com>
2017-05-16 13:37:28 +0100

    feature(index.html, index.js, routes.js): modify files include required scripts and references to index.html modify index.js import required packages and files modify routes.js add routes to newly created pages [#145350351]

--- client/index.js
+++ client/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import store from './store/configureStore';
 
+// const store = configureStore();
 
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
 
--- client/routes.js
+++ client/routes.js
@@ -3,12 +3,23 @@ import { Route, IndexRoute } from 'react-router';
 import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import DocumentsPage from './components/document/DocumentsPage';
+import AddDocumentsPage from './components/document/AddDocumentsPage';
+import DocumentsListPage from './components/document/DocumentListPage';
+import LoginPage from './components/login/LoginPage';
+import SingUpPage from './components/signup/SignUpPage';
+import UsersPage from './components/user/UsersPage';
+import SearchPage from './components/search/SearchPage';
+
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomePage} />
-    <Route path="document" component={DocumentsPage} />
+    <Route path="login" component={LoginPage} />
+    <Route path="signUp" component={SingUpPage} />
+    <Route path="addDocument" component={AddDocumentsPage} />
+    <Route path="document" component={DocumentsListPage} />
+    <Route path="user" component={UsersPage} />
+    <Route path="search" component={SearchPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
