97babd08bd338f2c445943d3b157425b10d9e823
Faith Mustapha <faith.mustapha@andela.com>
2017-05-16 13:27:57 +0100

    feature(components/common/Footer.js, Header.js, Layout.js): add footer component to project modify header component add side nav modify Layout component add header class name and footer [#145350351]

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './common/Header';
+import Footer from './common/Footer';
 
 class Layout extends React.Component {
   render() {
     return (
-      <div className="">
+      <div className="header">
         <Header />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
 }
+
 
 Layout.propTypes = {
   children: React.PropTypes.object.isRequired
--- client/components/common/Header.js
+++ client/components/common/Header.js
@@ -6,7 +6,10 @@ const Header = () => (
     <div id="nav-support">.</div>
     <nav>
       <div className="nav-wrapper teal darken-4">
-        <IndexLink to="/" activeClassName="brand-logo">ODAHI DMS</IndexLink>
+        <a href="#" data-activates="slide-out" className="button-collapse">
+          <i>menu</i>
+        </a>
+        <IndexLink page-padding to="/" activeClassName="brand-logo">ODAHI DMS</IndexLink>
         {/* <form>
           <div className="right input-field">
             <input id="search" type="search" required></input>
@@ -16,16 +19,19 @@ const Header = () => (
           </div>
         </form>*/}
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="Login.js">Login</a></li>
+          <li><Link to="/login" activeClassName="active">Login</Link></li>
+          <li><Link to="/signup" activeClassName="active">SignUp</Link></li>
           <li><Link to="/document" activeClassName="active">Documents</Link></li>
+          <li><Link to="/user" activeClassName="active">Users</Link></li>
+          <li><Link to="/search" activeClassName="active">Search</Link></li>
           <li><a href="badges.html">Components</a></li>
           <li><a href="https://github.com/andela-fmustapha/document-management">GitHub</a></li>
           <Link to="/about" className="waves-effect waves-light btn">Learn More</Link>
         </ul>
-        </div>
+      </div>
     </nav>
     <div id="nav-support">.</div>
-    </div>
+  </div>
   );
 
 export default Header;
