99dd259daa2bc25fcd32ab5661e6e16af371e6dc
Faith Mustapha <faith.mustapha@andela.com>
2017-06-12 19:23:40 +0100

    feature(Role.js, User.js): modify files [#146514255]

---
+++ server/controllers/Role.js
@@ -0,0 +1,114 @@
+import db from '../models/';
+
+const allRoles = ['admin', 'regular'];
+const Roles = db.Role;
+const Users = db.User;
+
+const Role = {
+  createRole(req, res) {
+    if (allRoles.indexOf(req.body.title) === -1) {
+      return res.status(403).json({ message: 'Invalid role title' });
+    }
+    Roles
+      .create(
+        req.body
+      )
+      .then(role => res.status(201).send({
+        role,
+        message: 'Role created succesfully'
+      }))
+      .catch((error) => {
+        res.status(400).send({
+          error,
+          message: 'Error creating new role'
+        });
+      });
+  },
+
+  listRole(req, res) {
+    return Roles
+      .findAll()
+      .then((role) => {
+        if (!role) {
+          return res.status(404).send({
+            message: 'Roles Not Found',
+          });
+        }
+        return res.status(200).send({ role });
+      })
+      .catch(error => res.status(400).send({
+        error,
+        message: 'Error retrieving all roles'
+      }));
+  },
+
+  retrieveRole(req, res) {
+    return Roles
+      .findById(req.params.id, {
+        include: [{
+          model: Users
+        }],
+      })
+      .then((role) => {
+        if (!role) {
+          return res.status(404).send({
+            message: 'Role Not Found',
+          });
+        }
+        return res.status(200).send({ role });
+      })
+      .catch(error => res.status(400).send({
+        error,
+        message: 'Error occured while retrieving role'
+      }));
+  },
+
+  updateRole(req, res) {
+    return Roles
+      .findById(req.params.id)
+      .then((role) => {
+        if (!role) {
+          return res.status(404).send({
+            message: 'Role Not Found',
+          });
+        }
+        return role
+          .update(req.body)
+          .then(() => res.status(200).send({
+            message: 'Role updated successfully.',
+            role
+          }))
+          .catch(error => res.status(400).send({
+            message: 'Role did not update successfully.',
+            error
+          }));
+      })
+      .catch(error => res.status(400).send({
+        error,
+        message: 'Error updating role'
+      }));
+  },
+
+  deleteRole(req, res) {
+    return Roles
+      .findById(req.params.id)
+      .then((role) => {
+        if (!role) {
+          return res.status(404).send({
+            message: 'Role Not Found',
+          });
+        }
+        return role
+          .destroy()
+          .then(() => res.status(200).send({
+            message: 'Role deleted successfully.'
+          }));
+      })
+      .catch(error => res.status(400).send({
+        message: 'Cannot delete role with users assigned to it.',
+        error
+      }));
+  },
+};
+
+export default Role;
